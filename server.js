6d2afc882c50eb9d025a81c1e7861dfce4d34d67
Naveen Muralidharan <naveenspec@gmail.com>
2022-11-09 18:19:06 -0500

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -6,10 +6,17 @@ const app = express();
 const PORT = 3000;
 
 // ROUTES
+
+// GREETING ROUTE
 app.get('/greeting/:name', (req, res)=>{
     res.send('Wow! Hello there, ' + req.params.name);
 })
 
+// TIP CALCULATOR ROUTE
+app.get('/tip/:total/:tipPercentage', (req, res)=>{
+    let tipAmount = (Number(req.params.total) * Number(req.params.tipPercentage))/100
+    res.send('Tip amount is $'+tipAmount)
+})
 
 
 
