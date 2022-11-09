7efb7364383f5186762e728263e38786c8fcbc40
Naveen Muralidharan <naveenspec@gmail.com>
2022-11-09 18:29:41 -0500

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -18,7 +18,15 @@ app.get('/tip/:total/:tipPercentage', (req, res)=>{
     res.send('Tip amount is $'+tipAmount)
 })
 
+// MAGIC ROUTE
+app.get('/magic/:question', (req, res)=>{
 
+    let eightBallResponses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+    let randomResponseIndex = Math.floor(Math.random() * (eightBallResponses.length-1))
+    let randomResponse = eightBallResponses[randomResponseIndex]
+
+    res.send(`${req.params.question} ? - Magic 8 ball response - <h1> ${randomResponse} </h1>`)
+})
 
 app.listen(PORT, ()=> {
     console.log('listenng on port '+PORT)
